d577ffcbef618a1836bc90516a8d57fc6b62cd30
RomuloQ <romuloqueiroz100@gmail.com>
2019-08-20 21:20:49 -0300

    init services and repositories

--- Microservices/Users/src/App.ts
+++ Microservices/Users/src/App.ts
@@ -1,1 +1,18 @@
 import "reflect-metadata";
+import * as express from "express";
+
+class App {
+
+    constructor() {
+        this.config()
+    }
+
+    config() {
+        const app = express()
+        app.listen(3001, () => console.log(`Example app listening on port ${3001}!`))
+    }
+}
+
+new App()
+
+
